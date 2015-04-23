edf468aacccb9973ed6b4ce1ad9dd2418bbd0e3b
grupoiris <digital@grupoiris.co>
2015-04-23 15:27:00 -0500

    refresh commit 6

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -57,8 +57,6 @@ $(document).ready(function() {
     return false;
  });
  
- $('#video').html('<div  class="embed-responsive embed-responsive-16by9">   <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/n4vlnY0dn4E"></iframe>  </div> <div class="embed-responsive embed-responsive-16by9">  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/3MBH85SWggg"></iframe> </div>');
- 
 });
 
 
@@ -92,4 +90,7 @@ $(function(){
  });
 });
 
-
+setTimeout(loadVideos,10000);
+function loadVideos(){
+	$('#video').html('<div  class="embed-responsive embed-responsive-16by9">   <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/n4vlnY0dn4E"></iframe>  </div> <div class="embed-responsive embed-responsive-16by9">  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/3MBH85SWggg"></iframe> </div>');	
+}
