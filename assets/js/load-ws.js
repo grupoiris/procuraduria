49a3c188bb404e7d36409e9c757e035fdcd8d733
grupoiris <digital@grupoiris.co>
2015-08-24 12:19:55 -0500

    final test

--- assets/js/load-ws.js
+++ assets/js/load-ws.js
@@ -4,7 +4,7 @@ var arrayComerciales 	=new Array();
 var SES = window.localStorage;
 var isPhonegap = false;
 function checkConnection() {
-	var onLine = isOnLine();
+	var onLine = isOnLine(); 
 	alert(onLine);
 	if( onLine == 'none' || onLine == 'des'){
 		SES.arrayToContents 	=new Array();
